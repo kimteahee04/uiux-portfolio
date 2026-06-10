1d4a9e305bd9099bb4a76e209327bf87ce2c29c4
Kimteahee <ktaehee6488@naver.com>
2026-06-10 17:05:29 +0900

    FIX - 사회 까지 지샵 & 제이쿼리 일단 오늘 끝

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$().on("", function(){
+    
+})
+
+
+
+
+// 목업
+$(window).on("scroll", function () {
+  // 스크롤값
+  let scrollTop = $(window).scrollTop();
+  // console.log(scrollTop);
+
+  // con02의 위치값
+  let con02Top = $(".con02").offset().top;
+  // console.log(con02Top);
+  base = -300;
+  if (scrollTop >= con02Top + base) {
+    $(".box .folio").addClass("active");
+  } else {
+    $(".box .folio").removeClass("active");
+  }
+});
+
+// con03
+$(window).on("scroll", function () {
+  // 스크롤값
+  let scrollTop = $(window).scrollTop();
+  // console.log(scrollTop);
+
+  // con03의 위치값
+  let con03Top = $(".con03").offset().top;
+  // console.log(con03Top);
+  base = -300;
+  if (scrollTop >= con03Top + base) {
+    $(".con03 .folio_1").addClass("active_1");
+  } else {
+    $(".con03 .folio_1").removeClass("active_1");
+  }
+});
